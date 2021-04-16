900e4fb9f6d2b81b895ac91f6d00d053ee153f26
Shohei Jinno <shohei.jinno@gmail.com>
2021-04-16 12:58:20 -0700

    Add templates.

--- dango/urls.py
+++ dango/urls.py
@@ -1,11 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
-
 from django.contrib import admin
 from django.urls import path
 
+from items import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.HomeView.as_view(), name='home')
 ]
 
 if settings.DEBUG:
