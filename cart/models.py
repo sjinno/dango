38b7c0960377b495105945b8abf3a19d6e54a011
Shohei Jinno <shohei.jinno@gmail.com>
2021-04-16 18:51:19 -0700

    (WIP) Work on models.py.

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=150)
+    slug = models.SlugField()
+    description = models.TextField()
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        'Order', related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f'{self.quantity} x {self.product.title}'
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_date = models.DateTimeField(auto_now=True)
+    ordered_date = models.DateTimeField(blank=True, null=True)
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.reference_number
+
+    @property
+    def reference_number(self):
+        return f'OORDER-{self.pk}'
