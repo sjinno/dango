303bf11bcc0c1de180adb6a6a75c52f29367099f
Shohei Jinno <shohei.jinno@gmail.com>
2021-04-23 20:38:22 -0700

    cart: Make subtotal and total sections.

--- cart/models.py
+++ cart/models.py
@@ -88,6 +88,24 @@ class Order(models.Model):
     def reference_number(self):
         return f'OORDER-{self.pk}'
 
+    def get_raw_subtotal(self):
+        total = 0
+        for order_item in self.items.all():
+            total += order_item.get_raw_total_item_price()
+        return total
+
+    def get_subtotal(self):
+        return '{:.2f}'.format(self.get_raw_subtotal() / 100)
+
+    def get_raw_total(self):
+        subtotal = self.get_raw_subtotal()
+        # total = subtotal - discounts + tax + delivery
+        return subtotal
+
+    def get_total(self):
+        total = self.get_raw_total()
+        return '{:.2f}'.format(total / 100)
+
 
 class Payment(models.Model):
     order = models.ForeignKey(
